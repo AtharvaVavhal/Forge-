97fe8029644b402bfdbe0beaee1f24bc14d0e7f2
AtharvaVavhal <atharvavavhal@gmail.com>
2026-08-25 22:52:27 +0530

    chaneg in footer 1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer() {
           <div className="mono text-sm text-paper/70">
             <p>
               hello@forgebuilds.in{" "}
-              <span className="text-paper/40">(TODO: confirm email)</span>
+              <span className="text-paper/40">(TODO: confirm the email)</span>
             </p>
             <p className="mt-1.5">
               +91 86006 97250{" "}
